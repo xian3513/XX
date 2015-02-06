59e7e5c8226108199847010b052d0b660273f18c
kt <kt@a2ndeiMac.local>
2015-02-06 23:43:43 +0800

    no

--- XX/SecondViewController.h
+++ XX/SecondViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  SecondViewController.h
 //  XX
 //
 //  Created by kt on 15/2/6.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface SecondViewController : UITableViewController
 
 @end
-
---
+++ XX/ThirdViewController.h
@@ -0,0 +1,13 @@
+//
+//  ThirdViewController.h
+//  XX
+//
+//  Created by kt on 15/2/6.
+//  Copyright (c) 2015年 kt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ThirdViewController : UITableViewController
+
+@end
